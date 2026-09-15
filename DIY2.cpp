9ffd46b3cf700f4bc551a5e4a10facaf15a9b299
Jithin-07 <jithinbpillai123@gmail.com>
2026-09-15 19:40:15 +0530

    Default argument

---
+++ DIY2.cpp
@@ -0,0 +1,25 @@
+//DIY2
+//DEFAULT ARGUMENT
+
+#include <iostream>
+using namespace std;
+
+int power(int base, int exp = 2)
+{
+    int result = 1;
+
+    for (int i = 1; i <= exp; i++)
+    {
+        result = result * base;
+    }
+
+    return result;
+}
+
+int main()
+{
+    cout << power(5) << endl;
+    cout << power(2, 10) << endl;
+
+    return 0;
+}
