4c6c1ad9f54aeb5d8f63f240b973eb1a16609f58
Jithin-07 <jithinbpillai123@gmail.com>
2026-09-15 19:44:50 +0530

    Rectangle Class

---
+++ DIY4.cpp
@@ -0,0 +1,52 @@
+//DIY4
+//RECTANGLE CLASS
+
+#include <iostream>
+using namespace std;
+
+class Rectangle
+{
+private:
+    float length;
+    float width;
+
+public:
+    void setLength(float l)
+    {
+        if (l >= 0)
+            length = l;
+        else
+            length = 0;
+    }
+
+    void setWidth(float w)
+    {
+        if (w >= 0)
+            width = w;
+        else
+            width = 0;
+    }
+
+    float area()
+    {
+        return length * width;
+    }
+
+    float perimeter()
+    {
+        return 2 * (length + width);
+    }
+};
+
+int main()
+{
+    Rectangle r;
+
+    r.setLength(10);
+    r.setWidth(5);
+
+    cout << "Area = " << r.area() << endl;
+    cout << "Perimeter = " << r.perimeter() << endl;
+
+    return 0;
+}
